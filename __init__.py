e6c71f3504dbf2b355ef044ab722f4a81322f15f
Santiago Parraguez <sanparra@uni-bremen.de>
2023-03-07 10:42:19 +0100

    Added inpaintn function to fill missing values

--- __init__.py
+++ __init__.py
@@ -13,13 +13,19 @@ Tool for smooth and replace missing data by interpolation using the discrete cos
 
 [1] Garcia D. (2010) Robust smoothing of gridded data in one and higher dimensions with missing values.
 Computational Statistics & Data Analysis, 2010; 54:1167-1178.
+
+[2] Wang, G., Garcia, D., Liu, Y., de Jeu, R., and Johannes Dolman, A. (2012). A three-dimensional gap filling method
+for large geophysical datasets: Application to global satellite soil moisture observations.
+Environmental Modelling & Software, 2012; 30:139-142.
 """
 __name__ = 'smoothn'
 __version__ = '1.1'
 __release__ = '2023-03'
 
 __all__ = [
-    'smoothn'
+    'smoothn',
+    'inpaintn'
 ]
 # ===== IMPORTS =======================================
 from .smoothn import smoothn
+from .inpaintn import inpaintn
---
+++ inpaintn.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Implementation of inpaintn(y)
+"""
+__all__ = [
+    'inpaintn'
+]
+# ====== IMPORT LIBRARIES ======
+import numpy as np
+from scipy.ndimage import label, sum_labels
+from .smoothn import smoothn
+
+# ================================================================================================
+
+def inpaintn(data, s=None, structure=None, robust=False, max_size=5, grid=None):
+    """
+    Fill missing values with a smoothn algorithm, based on the cosine discrete transform.
+    Returns the filled array.
+
+    Parameters
+    ----------
+    data : np.ma.MaskedArray
+        n-dimensional data to be filled.
+    s : None or float
+        Smoothing parameter, calculated automatically if not provided to minimize GCV score.
+    structure : int or tuple
+        Structure to count empty spaces. See scipy.ndimage.label function.
+    robust : bool
+        Indicates if a robust iteration is executed to avoid outliers.
+    max_size : int
+        Maximum number of pixels in hole to be filled.
+    grid : tuple, list
+        Grid dimensions, assumed regular [np.ones(data.ndim)] if not given.
+    """
+    if structure is None:
+        structure = tuple([3]*data.ndim)
+    labeled, n_labels = label(data.mask, structure=np.ones(structure, dtype=np.int16))
+    sizes = sum_labels(np.ones_like(data), labeled, index=range(n_labels + 1))
+    mask_size = sizes <= max_size
+    mask_clean = mask_size[labeled]
+
+    z = data.copy()
+    z[mask_clean] = smoothn(data, s, robust=robust, di=grid)[mask_clean]
+
+    return z
+
+
